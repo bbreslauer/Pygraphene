4a8f8ae8f134f244956d94bb09798a0b6e297a95
bbreslauer <bbreslauer@gmail.com>
2011-08-02 09:25:25 -0400

    Added more documentation. Added Figure.delPlot() method.

--- datapair.py
+++ datapair.py
@@ -6,9 +6,29 @@ from axis import Axis
 from marker import *
 
 class DataPair(object):
+    """
+    Represents a 2-D set of data. Contains the X and Y data, pointers to the
+    x and y axes, and maintains the lines and markers that are drawn.
+    """
     
-
+# TODO should add line and marker visible attributes, so that it is easier and more
+# efficient to disable drawing of either one
     def __init__(self, backend, x, y, xaxis, yaxis, lineArgs={}, markerArgs={}):
+        """
+        x, y
+            lists of something that can be plotted. Normally these are numbers, but
+            they can be anything that Axis.mapDataToPlot(), min(), and max() can
+            interpret.
+        xaxis, yaxis
+            the Axis instances that this data will be drawn in reference to. Usually
+            xaxis is the abscissa and yaxis is the ordinate, but they can be reversed.
+
+        lineArgs
+            Keyword arguments for the line segments that are drawn.
+
+        markerArgs
+            Keyword arguments for the markers that are drawn.
+        """
 
         self._backend = backend
 
@@ -22,48 +42,64 @@ class DataPair(object):
         self.setLineArgs(**lineArgs)
         self.setMarkerArgs(**markerArgs)
 
-
-
     def setX(self, x):
+        """Set the x data."""
         if isinstance(x, list):
             self._x = x
 
     def setY(self, y):
+        """Set the y data."""
         if isinstance(y, list):
             self._y = y
 
     def setXAxis(self, xaxis):
+        """Set the x axis."""
         if isinstance(xaxis, Axis):
             self._xaxis = xaxis
         else:
             self._xaxis = None
 
     def setYAxis(self, yaxis):
+        """Set the y axis."""
         if isinstance(yaxis, Axis):
             self._yaxis = yaxis
         else:
             self._yaxis = None
 
     def setLineArgs(self, **args):
+        """Set the line arguments."""
         self._lineArgs.update(args)
 
     def setMarkerArgs(self, **args):
+        """Set the marker arguments."""
         self._markerArgs.update(args)
 
     def maxXValue(self):
+        """Get the maximum value in the x data."""
         return max(self._x)
 
     def maxYValue(self):
+        """Get the maximum value in the y data."""
         return max(self._y)
 
     def minXValue(self):
+        """Get the minimum value in the x data."""
         return min(self._x)
 
     def minYValue(self):
+        """Get the minimum value in the y data."""
         return min(self._y)
 
-
+# TODO making the lines and markers should be separated out from
+# actually drawing them, and maybe we should subclass Artist, as is done with Axis
     def draw(self, *args, **kwargs):
+        """
+        Make the lines and markers and draw them to the Figure.
+
+        Note: This does not actually override Artist.draw() because DataPair does
+        not subclass Artist.
+        """
+
         ox = self._xaxis._ox
         oy = self._xaxis._oy
 
@@ -73,8 +109,10 @@ class DataPair(object):
         lineSegments = []
         markers = []
 
+        # Default marker type
         markerClass = CircleMarker
 
+        # Make the line segments
         for i in range(min(len(xPlotCoords), len(yPlotCoords)) - 1):
             line = Line(self._backend, **self._lineArgs)
             line.setPoints( xPlotCoords[i],
@@ -88,11 +126,11 @@ class DataPair(object):
             lineSegments.append(line)
             line.draw()
 
+        # Make the markers
         for x, y in zip(xPlotCoords, yPlotCoords):
             marker = markerClass(self._backend, **self._markerArgs)
             marker.setOrigin(ox, oy)
             marker.setPosition(x, y)
             markers.append(marker)
             marker.draw()
-
 
--- figure.py
+++ figure.py
@@ -8,8 +8,21 @@ from text import *
 
 
 class Figure(object):
+    """
+    Represents a figure that is being drawn on a canvas. The canvas is stored
+    as a backend, which must be a subclass of BackendBase. The Figure can contain
+    an unlimited number of Plots.
+
+    One Plot being designated as the current Plot at any given time. This is used
+    mainly for the non-OO plotter interface.
+    """
 
     def __init__(self, width, height):
+        """
+        width, height
+            The width and height of the figure, in pixels.
+        """
+
         self._backend = Qt4PySideBackend(width, height)
 
         self._title = Text(self._backend)
@@ -20,9 +33,26 @@ class Figure(object):
         self._currentPlot = None  # index of the current plot
 
     def addPlot(self, plot):
+        """
+        Add a Plot to the Figure and set it as current.
+        """
         self._plots.append(plot)
         self._currentPlot = len(self._plots) - 1
 
+    def delPlot(self, plot):
+        """
+        Delete the given plot from the Figure and set the current
+        Plot to the first one, if one exists.
+        """
+
+        try:
+            self._plots.remove(plot)
+            if len(self._plots) == 0:
+                self._currentPlot = None
+            else:
+                self._currentPlot = 0
+        except:
+            pass
 
     def getCurrentPlot(self):
         """
@@ -37,8 +67,9 @@ class Figure(object):
 
     def setCurrentPlot(self, plot):
         """
-        Check if fig already exists. If it does, then set it as active. If not,
-        then add it to the figures and set it as active.
+        Check if plot is already attached to this Figure. If it is, then
+        set it to be the current plot. If it is not, then add it to the
+        Figure and set it as the current plot.
         """
         
         if isinstance(plot, Plot):
@@ -49,18 +80,32 @@ class Figure(object):
                 self.addPlot(plot)
 
     def width(self):
+        """Return the width of the Figure."""
         return self._backend._scene.width()
 
     def height(self):
+        """Return the height of the Figure."""
         return self._backend._scene.height()
 
     def setTitle(self, title):
+        """
+        Set the title label to title.
+
+        title can be either a str or a Text object. If it is a str, then
+        then current label's text is updated. If it is a Text object, then
+        the current label is replaced with title.
+        """
+
         if isinstance(title, Text):
             self._title = title
         elif isinstance(title, str):
             self._title.setKwargs(text=title)
 
     def draw(self):
+        """
+        Show the backend, draw all plots, and draw the Figure title.
+        """
+
         self._backend.show()
 
         for p in self._plots:
@@ -69,13 +114,18 @@ class Figure(object):
         self._title.draw()
         
     def deleteAllPlots(self):
+        """
+        Remove all plots from the Figure.
+        """
+
         for plot in self._plots:
             del plot
         self._plots = []
         self._currentPlot = None
 
     def clear(self):
+        """
+        Wipe the canvas clean.
+        """
         self._backend.clear()
-
-
 
